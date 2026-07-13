fa00b523516dc51c9025f93caf8dc39185e90509
Phạm Tuấn <phamngoctuan9d1112@mail.com>
2026-07-13 11:37:38 +0700

    djf;djfkaldjfa

--- app/auth/callback/route.ts
+++ app/auth/callback/route.ts
@@ -1,5 +1,7 @@
 import { NextResponse } from 'next/server'
-import { createClient } from '@/utils/supabase/server' // Đường dẫn này sẽ khớp với file tạo ở Bước 2
+import { createClient } from "@/utils/supabase/client";
+
+const supabase = createClient(); // Đường dẫn này sẽ khớp với file tạo ở Bước 2
 
 export async function GET(request: Request) {
   const { searchParams, origin } = new URL(request.url)
