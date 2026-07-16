86498456e2a9af063e288c8ae4ac896a2bceee98
Phạm Tuấn <phamngoctuan9d1112@mail.com>
2026-07-16 18:23:02 +0700

    JFk;JFkdf

--- app/components/battle/QuestionCard.tsx
+++ app/components/battle/QuestionCard.tsx
@@ -3,34 +3,27 @@
 import MathText from "../MathText";
 
 type QuestionCardProps = {
-
-    question:string;
-
+    question: any;
 };
 
 export default function QuestionCard({
-
     question,
+}: QuestionCardProps) {
 
-}:QuestionCardProps){
+    if (!question) return null;
 
-return(
-
-<div
-className="
-max-w-3xl
-mx-auto
-mt-8
-text-center
-text-2xl
-font-bold
-"
->
-
-<MathText text={question}/>
-
-</div>
-
-);
-
+    return (
+        <div
+            className="
+            max-w-3xl
+            mx-auto
+            mt-8
+            text-center
+            text-2xl
+            font-bold
+            "
+        >
+            <MathText text={question.question} />
+        </div>
+    );
 }
