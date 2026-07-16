c67eb54b37356dc3e06aa858506e08d61bd3e6ba
Phạm Tuấn <phamngoctuan9d1112@mail.com>
2026-07-16 13:16:01 +0700

    JFka;JFkadsjf

--- app/components/StoryModal.tsx
+++ app/components/StoryModal.tsx
@@ -75,6 +75,7 @@ export default function StoryModal(
     <div className="relative flex items-center justify-center h-full">
 
             <motion.div
+            
     initial={{
         opacity: 0,
         scale: 0.9,
@@ -94,16 +95,49 @@ export default function StoryModal(
     max-w-[92%]
     rounded-3xl
     border-4
-    border-yellow-500
+    border-[#d9b96f]
+    shadow-yellow-300/20
+    overflow-hidden
     bg-[#081B39]
     shadow-2xl
     p-10
     "
 >
+    <div
+className="
+absolute
+top-0
+left-0
+right-0
+h-2
+bg-gradient-to-r
+from-yellow-400
+via-yellow-200
+to-yellow-400
+"
+/>
+
+<div className="absolute top-5 left-5 text-yellow-300 text-3xl">
+✦
+</div>
+
+<div className="absolute top-5 right-5 text-yellow-300 text-3xl">
+✦
+</div>
+
+<div className="absolute bottom-5 left-5 text-yellow-300 text-3xl">
+✦
+</div>
+
+<div className="absolute bottom-5 right-5 text-yellow-300 text-3xl">
+✦
+</div>
 
                 <h2
     className="
-    text-6xl
+    text-7xl
+    drop-shadow-[0_0_15px_rgba(255,230,120,0.7)]
+    font-serif
     font-extrabold
     text-center
     text-yellow-100
@@ -116,6 +150,21 @@ export default function StoryModal(
 
                 </h2>
 
+                <div
+className="
+mx-auto
+mb-8
+
+w-60
+h-[3px]
+
+bg-gradient-to-r
+from-transparent
+via-yellow-300
+to-transparent
+"
+/>
+
                 <div className="grid grid-cols-2 gap-10 items-center">
 
     <motion.img
@@ -125,19 +174,24 @@ export default function StoryModal(
     x: -40,
 }}
 
+
 animate={{
-    opacity: 1,
-    x: 0,
+    opacity:1,
+    x:0,
+    scale:[1,1.05,1]
 }}
 
 transition={{
-    duration: 0.6,
+    duration:8,
+    repeat:Infinity
 }}
         className="
         w-full
         rounded-xl
         shadow-2xl
-        border-2
+        border-4
+        shadow-yellow-200/20
+        rounded-2xl
         border-yellow-400
         "
     />
@@ -159,7 +213,9 @@ transition={{
 }}
         className="
         whitespace-pre-line
-        text-2xl
+        text-[28px]
+        font-light
+        tracking-wide
         leading-10
         text-gray-100
         "
@@ -180,23 +236,37 @@ whileTap={{
                     onClick={onNext}
 
                     className="
-mt-10
+mt-12
+
 mx-auto
+
 block
-px-16
+
+px-20
+
 py-4
-rounded-xl
-text-2xl
+
+rounded-2xl
+
 font-bold
-bg-gradient-to-r
-from-blue-700
-to-blue-500
-hover:scale-105
-transition
-text-white
+
+text-2xl
+
+bg-gradient-to-b
+
+from-[#3f87ff]
+
+to-[#2056d9]
+
 border-2
+
 border-yellow-300
-shadow-xl
+
+shadow-lg
+
+shadow-blue-400/40
+
+text-white
 "
 
                 >
@@ -204,6 +274,17 @@ shadow-xl
                     Tiếp tục
 
                 </motion.button>
+
+                <div
+    className="
+    absolute
+    w-[1150px]
+    h-[700px]
+    rounded-full
+    bg-blue-400/20
+    blur-[120px]
+    "
+/>
 
             </motion.div>
 
