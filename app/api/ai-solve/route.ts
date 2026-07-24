4ef1310740e338dc15aa57bd2228e4edc7f6446f
Phạm Tuấn <phamngoctuan9d1112@mail.com>
2026-07-24 10:40:27 +0700

    FJaldjjj

--- app/api/ai-solve/route.ts
+++ app/api/ai-solve/route.ts
@@ -1,14 +1,35 @@
 import OpenAI from "openai";
-
+import { createClient } from "@supabase/supabase-js";
+import crypto from "crypto";
 const client = new OpenAI({
   apiKey: process.env.GROQ_API_KEY!,
   baseURL: "https://api.groq.com/openai/v1",
 });
-
+const supabase = createClient(
+  process.env.NEXT_PUBLIC_SUPABASE_URL!,
+  process.env.SUPABASE_SERVICE_ROLE_KEY!
+);
 export async function POST(req: Request) {
   try {
     const { question, options } = await req.json();
+const questionHash = crypto
+  .createHash("sha256")
+  .update(question + JSON.stringify(options))
+  .digest("hex");
+  const { data: cached } = await supabase
+  .from("ai_answers")
+  .select("answer")
+  .eq("question_hash", questionHash)
+  .single();
 
+if (cached) {
+  console.log("CACHE HIT");
+
+  return Response.json({
+    success: true,
+    answer: cached.answer,
+  });
+}
     const prompt = `
 Bạn là giáo viên Toán THPT Việt Nam.
 
@@ -42,7 +63,13 @@ Yêu cầu:
 
     const answer =
       completion.choices[0]?.message?.content ?? "Không có phản hồi.";
-
+await supabase
+  .from("ai_answers")
+  .insert({
+    question_hash: questionHash,
+    question,
+    answer,
+  });
     return Response.json({
       success: true,
       answer,
